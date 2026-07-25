9679302dd61ed9e2f14502b9d08d05de05ca0c31
Los Finkos <finkdavif@aon.at>
2026-07-25 18:28:18 +0200

    v3

--- main.py
+++ main.py
@@ -2,45 +2,52 @@ def notizen():
     print()
     print("=== NOTIZEN ===")
     print("Hier entstehen später deine Notizen.")
+    input("\nENTER drücken...")
 
 
 def passwortgenerator():
     print()
     print("=== PASSWORTGENERATOR ===")
     print("Hier entsteht später der Passwortgenerator.")
+    input("\nENTER drücken...")
 
 
 def rechner():
     print()
     print("=== RECHNER ===")
     print("Hier entsteht später der Rechner.")
+    input("\nENTER drücken...")
 
 
-print("=" * 30)
-print("     PIOS Academy")
-print("=" * 30)
+while True:
 
-print()
-print("1. Notizen")
-print("2. Passwortgenerator")
-print("3. Rechner")
-print("0. Beenden")
+    print("\n" * 3)
+    print("=" * 35)
+    print("         PIOS Academy")
+    print("=" * 35)
 
-print()
+    print()
+    print("1. Notizen")
+    print("2. Passwortgenerator")
+    print("3. Rechner")
+    print("0. Beenden")
+    print()
 
-auswahl = input("Bitte wähle einen Menüpunkt: ")
+    auswahl = input("Bitte wähle einen Menüpunkt: ")
 
-if auswahl == "1":
-    notizen()
+    if auswahl == "1":
+        notizen()
 
-elif auswahl == "2":
-    passwortgenerator()
+    elif auswahl == "2":
+        passwortgenerator()
 
-elif auswahl == "3":
-    rechner()
+    elif auswahl == "3":
+        rechner()
 
-elif auswahl == "0":
-    print("Programm wird beendet.")
+    elif auswahl == "0":
+        print("\nProgramm wird beendet.")
+        break
 
-else:
-    print("Ungültige Eingabe.")
+    else:
+        print("\nUngültige Eingabe.")
+        input("ENTER drücken...")
