1b981ec37a508fa659897e1f8110ae2e3e2d59b1
Los Finkos <finkdavif@aon.at>
2026-07-25 18:25:15 +0200

    v3

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+def notizen():
+    print()
+    print("=== NOTIZEN ===")
+    print("Hier entstehen später deine Notizen.")
+
+
+def passwortgenerator():
+    print()
+    print("=== PASSWORTGENERATOR ===")
+    print("Hier entsteht später der Passwortgenerator.")
+
+
+def rechner():
+    print()
+    print("=== RECHNER ===")
+    print("Hier entsteht später der Rechner.")
+
+
 print("=" * 30)
 print("     PIOS Academy")
 print("=" * 30)
 
 print()
-
 print("1. Notizen")
 print("2. Passwortgenerator")
 print("3. Rechner")
@@ -13,19 +30,17 @@ print()
 
 auswahl = input("Bitte wähle einen Menüpunkt: ")
 
-print()
-
 if auswahl == "1":
-    print("Notizen werden geöffnet...")
+    notizen()
 
 elif auswahl == "2":
-    print("Passwortgenerator wird geöffnet...")
+    passwortgenerator()
 
 elif auswahl == "3":
-    print("Rechner wird geöffnet...")
+    rechner()
 
 elif auswahl == "0":
     print("Programm wird beendet.")
 
 else:
-    print("Ungültige Eingabe!")
+    print("Ungültige Eingabe.")
