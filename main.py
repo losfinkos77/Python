8cd65760c840664ed18af8bec33ec595f794f704
Los Finkos <finkdavif@aon.at>
2026-07-25 18:12:31 +0200

    Erste Python-Version

---
+++ main.py
@@ -0,0 +1,18 @@
+print("=" * 30)
+print("     PIOS Academy")
+print("=" * 30)
+
+print()
+
+print("1. Notizen")
+print("2. Passwortgenerator")
+print("3. Rechner")
+print("0. Beenden")
+
+print()
+
+auswahl = input("Bitte wähle einen Menüpunkt: ")
+
+print()
+
+print("Du hast gewählt:", auswahl)
